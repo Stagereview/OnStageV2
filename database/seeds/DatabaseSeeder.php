bd8a449fbb1dd8c36ee64da4a81b8c1b1b786510
Peter van der Net <petervandernet@gmail.com>
2019-09-17 10:53:37 +0200

    Adjusted database + added foreign key

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,6 +28,8 @@ class DatabaseSeeder extends Seeder
             'email' => '12345678@mydavinci.nl',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'email_verified_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         // Fill the database with random users.
