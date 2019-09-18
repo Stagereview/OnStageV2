3910d090a5c166feeaa3b18d2b522d7506de9e4f
Peter van der Net <petervandernet@gmail.com>
2019-09-18 09:59:42 +0200

    Text changes after feedback

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Registeer') }}</div>
+                <div class="card-header">{{ __('Registreer') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -83,7 +83,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary ">
-                                    {{ __('Registeer') }}
+                                    {{ __('Registreer') }}
                                 </button>
                             </div>
                         </div>
