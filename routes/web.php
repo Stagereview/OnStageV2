c411cca8a9dcb72b518dffe40e85f824fccfa446
Peter van der Net <petervandernet@gmail.com>
2019-10-02 11:58:36 +0200

    Changed routing

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 // Company
 Route::resource('company', 'CompanyController');
 
-Route::get('/company/search/{company}', 'CompanyController@search')->name('company.search')
+Route::get('/company/search/{company}', 'CompanyController@search')->name('company.search');
 
 Route::fallback('CompanyController@index');
